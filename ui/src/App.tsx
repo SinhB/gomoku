25b780753cb714609596e8262d82ce8c38c2e01d
Quentin <synkx@hotmail.fr>
2022-10-28 16:53:07 +0200

    logo lol

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,13 @@
 import Board from "./components/Board";
+import Pawn from "./components/Pawn";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Gomoku</p>
+        <Pawn color="black" id={0} />
+        <Pawn color="white" id={0} />
       </header>
       <body>
         <section className="board-section">
--- ui/src/components/Pawn.tsx
+++ ui/src/components/Pawn.tsx
@@ -1,5 +1,5 @@
 export default function Pawn(props: { color: string; id: number }) {
   const { color, id } = props;
 
-  return <div className={`pawn ${color}`}>{id}</div>;
+  return <span className={`pawn ${color}`}>{id > 0 ? id : ""}</span>;
 }
