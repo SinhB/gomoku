db8037c765a3d077252947d09a7c46a465584a87
Yann <yannbecret@gmail.com>
2022-10-31 14:43:18 +0100

    clean alg but still docstring to write

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,11 +1,9 @@
-import numpy as np
-from numba import njit, types
+from numba import njit
 
 from src.heuristic import get_numba_sequence_frequences
 from src.utils import timeit
 
 
-# @timeit
 def minimax(state, alpha, beta, depth, is_maximiser):
     if depth == 0 or state.is_finished():
         return evaluate_state(state._board, state.color.swap().name)
@@ -31,10 +29,9 @@ def minimax(state, alpha, beta, depth, is_maximiser):
                 break
             return best_eval
 
+
 @njit
 def evaluate_state(board, color):
-    # print(f"COLOR in EVALUATE: {color}")
-    # print(f"SELF.COLOR in EVALUATE: {state.color}")
     current_threats = get_numba_sequence_frequences(board)
     priority_keys = (
         "five",
@@ -48,10 +45,10 @@ def evaluate_state(board, color):
         "simple_two",
     )
 
+    # Get the first 'counter' type of sequence to make the score
     counter = 2
     score = 0
     for i, key in enumerate(priority_keys):
-        # print(f"IN LOOP: for {key} {current_threats[color][key]}")
         if counter == 0:
             return score
         if current_threats[color][key] != 0:
