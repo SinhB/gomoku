c73c163645fbb5c41a60faddcbfa4d10ad4c210a
Quentin <synkx@hotmail.fr>
2022-10-31 14:03:29 +0100

    fix conflicts

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,19 @@
-import "./App.css";
 import Board from "./components/Board";
+import Pawn from "./components/Pawn";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Gomoku</p>
-        <Board />
+        <Pawn color="black" id={0} />
+        <Pawn color="white" id={0} />
       </header>
+      <body>
+        <section className="board-section">
+          <Board />
+        </section>
+      </body>
     </div>
   );
 }
--- ui/src/components/Row.tsx
+++ ui/src/components/Row.tsx
@@ -1,35 +1,38 @@
 import { useEffect, useState } from "react";
 import { IPawn } from "../types/general";
-import EmptySpace from "./EmptySpace";
+import { areArraysEqual } from "../utils";
 import Pawn from "./Pawn";
 
 export default function Row(props: { col: number }) {
-  const [pawns, setPawns] = useState<IPawn[]>();
+  const [pawns, setPawns] = useState<IPawn[]>([]);
+  const { col } = props;
 
   useEffect(() => {
-    let originalPawns = [{ id: 1, color: "black", coordinates: [0, 0] }];
+    let originalPawns = [{ id: 1, color: "black", coordinates: [2, 5] }];
     setPawns(originalPawns);
   }, []);
 
-  const { col } = props;
+  function handleClickedCoordinates(coordinate: any) {
+    console.log(coordinate);
+  }
 
   return (
-    <div>
-      {[...Array(19)].map((cell, i) => (
-        <span key={cell}>
-          {pawns?.map((pawn, i) => (
-            <span key={pawn.id}>
-              {pawn.id == 1 ? (
-                <Pawn color="black" />
-              ) : (
-                <>
-                  <EmptySpace />
-                </>
-              )}
-            </span>
-          ))}
-        </span>
+    <>
+      {[...Array(19)].map((currentElement, i) => (
+        <div
+          className="square"
+          onClick={() => handleClickedCoordinates([col, i])}
+        >
+          <div>
+            {pawns?.map(
+              (pawn) =>
+                areArraysEqual(pawn.coordinates, [col, i]) && (
+                  <Pawn color={pawn.color} id={pawn.id} />
+                )
+            )}
+          </div>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
