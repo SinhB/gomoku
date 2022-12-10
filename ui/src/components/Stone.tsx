731ce3737e64d78fa3ecdbba6952d49d121170f8
Lou CHANCRIN <lchancri@e2r10p7.clusters.42paris.fr>
2022-12-10 14:00:53 +0100

    Remove unused folder

--- ui/src/components/Stone.tsx
+++
@@ -1,7 +0,0 @@
-import { IStone } from "../types/general";
-
-export default function Stone(props: IStone) {
-  const { color, id } = props;
-
-  return <span className={`stone ${color}`}>{id >= 0 ? id : ""}</span>;
-}
--- ui/src/utils.tsx
+++
@@ -1,6 +0,0 @@
-export function areArraysEqual(array1: any[], array2: any[]) {
-  return (
-    array1.length === array2.length &&
-    array2.every((value, index) => value === array1[index])
-  );
-}
