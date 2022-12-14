82b4ba2e60d5a17c33a5000fb2f3b362cdef1a72
Lou CHANCRIN <louchancrin@hotmail.com>
2022-12-14 04:47:30 +0100

    WIP: attempt refont of check side

--- python_server/get_threats.py
+++ python_server/get_threats.py
@@ -6,129 +6,126 @@ import numpy as np
 from numba import njit, prange, int64, typeof
 from numba.types import bool_
 
-print_values = True
-
 @njit("(int64)(int64)", fastmath=True)
 def eat_value(eat_number):
     if eat_number == 0:
-        return 0
+        return 9
     elif eat_number == 1:
-        return 1_000 
+        return 100_000 
     elif eat_number == 2:
-        return 100_000
+        return 500_000
     elif eat_number == 3:
         return 1_000_000
     elif eat_number == 4:
         return 10_000_000
     return 100_000_000
 
-multiplicator_five = 50_000_000
-multiplicator_enemy_five = 25_000_000
-
-multiplicator_enemy_open_four = 50_500_000
-multiplicator_enemy_semi_closed_four = 50_000_000
-
-multiplicator_open_four = 10_000_000
-
-# multiplicator_enemy_open_four = 2_500_000
-# multiplicator_enemy_semi_closed_four = 2_000_000
-multiplicator_enemy_open_three = 8_000_000
-multiplicator_open_three = 4_000_000
-
-# multiplicator_enemy_open_three = 1_000_000
-# multiplicator_open_three = 500_000
+multiplicator_five = 500_000_000
+multiplicator_enemy_five = 250_000_000
+multiplicator_enemy_open_four = 25_000_000
+multiplicator_enemy_semi_closed_four = 20_000_000
+multiplicator_enemy_open_three = 1_000_000
+multiplicator_open_four = 1_500_000
+multiplicator_open_three = 100_000
 multiplicator_semi_closed_four = 20_000
-multiplicator_enemy_semi_closed_three = 0
-multiplicator_semi_closed_three = 0
-# multiplicator_enemy_semi_closed_three = 10_000
-# multiplicator_semi_closed_three = 5_000
-multiplicator_open_two = 20
-multiplicator_semi_closed_two = 0
-# multiplicator_semi_closed_two = 5
-multiplicator_enemy_semi_closed_two = 0
-# multiplicator_enemy_semi_closed_two = 10
-multiplicator_enemy_open_two = 10
+multiplicator_enemy_semi_closed_three = 10_000
+multiplicator_semi_closed_three = 5_000
+multiplicator_open_two = 50
+multiplicator_semi_close_two = 5
+multiplicator_enemy_semi_close_two = 10
+multiplicator_enemy_open_two = 100
 
-@njit("Tuple((int64, int64, boolean, boolean, boolean, boolean, int64, int64))(int64[:], int64)", fastmath=True)
+# # Player series multiplicator
+# multiplicator_closed_two = 10
+# multiplicator_semi_close_two = 50
+# multiplicator_open_two = 500
+
+# multiplicator_closed_three = 20
+# multiplicator_semi_closed_three = 1_000
+# multiplicator_open_three = 10_000
+
+# multiplicator_closed_four = 40
+# multiplicator_semi_closed_four = 20_000
+# multiplicator_open_four = 100_000
+
+# multiplicator_five = 100_000_000
+
+# # Block enemy series multiplicator
+# multiplicator_enemy_closed_two = 20
+# multiplicator_enemy_semi_close_two = 100
+# multiplicator_enemy_open_two = 1000
+
+# multiplicator_enemy_closed_three = 40
+# multiplicator_enemy_semi_closed_three = 2_000
+# multiplicator_enemy_open_three = 400_000
+
+# multiplicator_enemy_closed_four = 80
+# multiplicator_enemy_semi_closed_four = 1_000_000
+# multiplicator_enemy_open_four = 5_000_000
+
+# multiplicator_enemy_five = 50_000_000
+
+# Eating move
+multiplicator_open_eat_move = 200
+
+@njit("Tuple((int64, int64, boolean, boolean, boolean, int64))(int64[:], int64)", fastmath=True)
 def check_side(side, player):
+    # Number of consecutive stone placed directly next to the one played
+    consecutive = 0
+    # Number of enemy consecutive stone placed directly next to the one played
+    consecutive_enemy = 0
+    # Number of consecutive stone separated by one zero from the consecutive ones
+    additional = 0
+    # Is there an empty space at the end of the last serie of stone
+    empty_space = False
+    # Eating enemy
+    eating_enemy = False
+    # Open eating move
+    open_eating_move = False
+
     is_consecutive = True
-    consecutive_value = False
-    consecutive_count = 0
-
-    is_additional = False
-    additional_value = False
-    additional_count = 0
-
-    is_blocked = False
-
+    check_eating_enemy = True
+    check_open_eating_move = True
     is_after_one_zero = False
-
-    empty_space = True
 
     for i in range(0, min(len(side), 6)):
         if is_consecutive:
-            if side[i] == 0:
+            if side[i] == player:
+                consecutive += 1
+            else:
                 is_consecutive = False
+
+        if (check_eating_enemy or check_open_eating_move) and not is_after_one_zero:
+            if side[i] == player and consecutive_enemy == 2:
+                # Return eating_move
+                return 0, 0, True, True, False, consecutive_enemy
+            elif side[i] == 0 and consecutive_enemy == 2:
+                # Return open_eating_move
+                return 0, 0, True, False, True, consecutive_enemy
+            elif side[i] == player * -1:
+                consecutive_enemy += 1
+            else:
+                check_eating_enemy = False
+                check_open_eating_move = False
+
+        if side[i] == 0:
+            if i == 0 or consecutive > 0 or consecutive_enemy > 0:
+                if not (consecutive == 0 and consecutive_enemy == 0):
+                    empty_space = True
+            if is_after_one_zero:
+                break
+            else:
                 is_after_one_zero = True
-            elif consecutive_value == False:
-                consecutive_value = side[i]
-                consecutive_count += 1
-            elif side[i] == consecutive_value:
-                consecutive_count += 1
-            else:
-                empty_space = False
-                is_additional = True
-                is_consecutive = False
-                additional_value = side[i]
-                additional_count += 1
-
-        elif is_after_one_zero:
-            if side[i] == 0:
-                break
-            else:
-                is_additional = True
-                additional_value = side[i]
-                additional_count += 1
+            check_eating_enemy = False
+            # check_open_eating_move = False
         
-        elif is_additional:
-            if side[i] == additional_value:
-                additional_count += 1
-            else:
-                break
-
-    if consecutive_value != False:
-        consecutive = consecutive_count if consecutive_value == player else 0
-        consecutive_enemy = consecutive_count if consecutive_value == player * -1 else 0
-    else:
-        consecutive = 0
-        consecutive_enemy = 0
-
-    if additional_value != False:
-        additional = additional_count if additional_value == player else 0
-        additional_enemy = additional_count if additional_value == player * -1 else 0
-    else:
-        additional = 0
-        additional_enemy = 0
-
-    print(player, consecutive_value, consecutive_count, is_after_one_zero, additional_value)
-    if consecutive_value == player * -1 and consecutive_count == 2:
-        open_eating_move = True if is_after_one_zero else False
-        if not is_after_one_zero and additional_value == player:
-            eating_enemy = True
-            empty_space = True
-        else:
-            eating_enemy = False
-        open_get_eat_move = False
-
-    elif consecutive_value == player and consecutive_count == 2 and is_after_one_zero:
-        open_get_eat_move = True
-
-    else:
-        eating_enemy = False
-        open_eating_move = False
-        open_get_eat_move = False
-
-    return consecutive, additional, empty_space, eating_enemy, open_eating_move, open_get_eat_move, consecutive_enemy, additional_enemy
+        if is_after_one_zero:
+            if side[i] == player:
+                additional += 1
+            # else:
+            #     break
+        
+    return consecutive, additional, empty_space, eating_enemy, open_eating_move, consecutive_enemy
 
 # @functools.cache
 # @njit("UniTuple(int64, 23)(int64[:], int64, int64)", fastmath=True)
@@ -136,116 +133,121 @@ def check_side(side, player):
 def check_line(line, starting_index, player, player_eat, enemy_eat):
     left = line[0:starting_index][::-1]
     right = line[starting_index+1:]
-    if print_values:
-        print(line)
-        print(left, right)
 
-    l_consecutive, l_additional, l_empty_space, l_eating_enemy, l_open_eating_move, l_open_get_eat_move, l_consecutive_enemy, l_additional_enemy = check_side(left, player)
-    if print_values:
-        print(l_consecutive, l_additional, l_empty_space, l_eating_enemy, l_open_eating_move, l_open_get_eat_move, l_consecutive_enemy, l_additional_enemy)
-    r_consecutive, r_additional, r_empty_space, r_eating_enemy, r_open_eating_move, r_open_get_eat_move, r_consecutive_enemy, r_additional_enemy = check_side(right, player)
-    if print_values:
-        print(r_consecutive, r_additional, r_empty_space, r_eating_enemy, r_open_eating_move, r_open_get_eat_move, r_consecutive_enemy, r_additional_enemy)
-        print("\n")
+    print(line)
+    print(left)
+    print(right)
 
-    total_consecutive = l_consecutive + r_consecutive
+    l_consecutive, l_additional, l_empty_space, l_eating_enemy, l_open_eating_move, l_consecutive_enemy = check_side(left, player)
+    print(l_consecutive, l_additional, l_empty_space, l_eating_enemy, l_open_eating_move, l_consecutive_enemy)
+    r_consecutive, r_additional, r_empty_space, r_eating_enemy, r_open_eating_move, r_consecutive_enemy = check_side(right, player)
+    print(r_consecutive, r_additional, r_empty_space, r_eating_enemy, r_open_eating_move, r_consecutive_enemy)
+    print("\n\n")
+
+    total_consecutive = l_consecutive + r_consecutive + 1
     total_consecutive_enemy = l_consecutive_enemy + r_consecutive_enemy
     total_empty_space = l_empty_space + r_empty_space
 
+    eat_move = l_eating_enemy + r_eating_enemy
+    open_eat_move = l_open_eating_move + r_open_eating_move
+    open_get_eat_move = 0
+    if l_consecutive == 2 and l_empty_space == False:
+        open_get_eat_move += 1
+    if r_consecutive == 2 and r_empty_space == False:
+        open_get_eat_move += 1
+
     # Player serie
-    semi_closed_two = 0
+    closed_two = 0
+    semi_close_two = 0
     open_two = 0
 
+    closed_three = 0
     semi_closed_three = 0
     open_three = 0
 
+    closed_four = 0
     semi_closed_four = 0
     open_four = 0
 
-    five = 0 if total_consecutive < 4 else 1
+    five = 0
 
+    if l_consecutive + l_additional == 2 and ((r_eating_enemy or r_empty_space) or l_empty_space):
+        open_three += 1
+    if r_consecutive + r_additional == 2 and ((l_eating_enemy or l_empty_space) or r_empty_space):
+        open_three += 1
+
+    if total_consecutive == 1:
+        if total_empty_space == 2:
+            open_two += 1
+        elif total_empty_space == 1:
+            semi_close_two += 1
+        else:
+            closed_two += 1
+
+    elif total_consecutive == 2:
+        if total_empty_space == 2:
+            open_three += 1
+        elif total_empty_space == 1:
+            semi_closed_three += 1
+        else:
+            closed_three += 1
+
+    elif total_consecutive == 3:
+        if total_empty_space == 2:
+            open_four += 1
+        elif total_empty_space == 1:
+            semi_closed_four += 1
+        else:
+            closed_four += 1
+
+    elif total_consecutive >= 4:
+        five += 1
+    
     # Enemy player serie
-    enemy_semi_closed_two = 0
+    enemy_closed_two = 0
+    enemy_semi_close_two = 0
     enemy_open_two = 0
 
+    enemy_closed_three = 0
     enemy_semi_closed_three = 0
     enemy_open_three = 0
 
+    enemy_closed_four = 0
     enemy_semi_closed_four = 0
     enemy_open_four = 0
 
-    enemy_five = 0 if total_consecutive_enemy < 4 else 1
+    enemy_five = 0
 
-    eat_move = l_eating_enemy + r_eating_enemy
-    open_eat_move = l_open_eating_move + r_open_eating_move
-    open_get_eat_move = l_open_get_eat_move + r_open_get_eat_move
-
-    if l_additional != 0 and l_consecutive + l_additional == 2 and l_empty_space == True:
-        open_three += 1
-
-    if l_additional != 0 and l_consecutive + l_additional == 3 and l_empty_space == True:
-        open_four += 1
-
-    if r_empty_space:
-        if r_additional != 0:
-            if r_consecutive + r_additional == 2:
-                open_three += 1
-            if r_consecutive + r_additional == 3:
-                open_four += 1
-        if r_additional_enemy != 0:
-            if r_consecutive_enemy + r_additional_enemy == 2:
-                enemy_open_three += 1
-            if r_consecutive_enemy + r_additional_enemy == 3:
-                enemy_open_four += 1
-
-    if l_empty_space:
-        if l_additional != 0:
-            if l_consecutive + l_additional == 2:
-                open_three += 1
-            if l_consecutive + l_additional == 3:
-                open_four += 1
-        if l_additional_enemy != 0:
-            if l_consecutive_enemy + l_additional_enemy == 2:
-                enemy_open_three += 1
-            if l_consecutive_enemy + l_additional_enemy == 3:
-                enemy_open_four += 1
-
-    if total_empty_space == 2:
-        # Open two
-        if total_consecutive == 1:
-            open_two += 1
-        if total_consecutive_enemy == 1:
+    if total_consecutive_enemy == 1:
+        if total_empty_space == 2:
             enemy_open_two += 1
-        # Open three
-        if total_consecutive == 2:
-            open_three += 1
-        if total_consecutive_enemy == 2:
+        elif total_empty_space == 1:
+            enemy_semi_close_two += 1
+        else:
+            enemy_closed_two += 1
+
+    elif total_consecutive_enemy == 2:
+        if total_empty_space == 1:
             enemy_open_three += 1
-        # Open four
-        if total_consecutive == 3:
-            open_four += 1
-        if total_consecutive_enemy == 3:
-            enemy_open_four += 1
-    elif total_empty_space == 1:
-        # Semi closed two
-        if total_consecutive == 1:
-            semi_closed_two += 1
-        if total_consecutive_enemy == 1:
-            enemy_semi_closed_two += 1
-        # Semi closed three
-        if total_consecutive == 2:
-            semi_closed_three += 1
-        if total_consecutive_enemy == 2:
+        elif total_empty_space == 0:
             enemy_semi_closed_three += 1
-        # Semi closed four
-        if total_consecutive == 3:
-            semi_closed_four += 1
-        if total_consecutive_enemy == 3:
+        else:
+            enemy_closed_three += 1
+
+    elif total_consecutive_enemy == 3:
+        if total_empty_space == 2:
+            enemy_open_four += 1
+        elif total_empty_space == 1:
             enemy_semi_closed_four += 1
+        else:
+            enemy_closed_four += 1
+
+    elif total_consecutive_enemy >= 4:
+        enemy_five += 1
 
     score = 1
     # Player series
-    score += semi_closed_two * multiplicator_semi_closed_two
+    score += semi_close_two * multiplicator_semi_close_two
     score += open_two * multiplicator_open_two
 
     score += semi_closed_three * multiplicator_semi_closed_three
@@ -257,7 +259,7 @@ def check_line(line, starting_index, player, player_eat, enemy_eat):
     score += five * multiplicator_five
 
     # Enemy series
-    score += enemy_semi_closed_two * multiplicator_enemy_semi_closed_two
+    score += enemy_semi_close_two * multiplicator_enemy_semi_close_two
     score += enemy_open_two * multiplicator_enemy_open_two
 
     score += enemy_semi_closed_three * multiplicator_enemy_semi_closed_three
@@ -273,11 +275,11 @@ def check_line(line, starting_index, player, player_eat, enemy_eat):
         # score += 100_000 + ((eat_move + player_eat + 1) ** 10)
 
     if open_eat_move:
-        score += eat_value(open_eat_move + player_eat)
+        score += eat_value(open_eat_move + player_eat) / 10
         # score += (open_eat_move + player_eat + 1) ** 7
 
     if open_get_eat_move:
-        score -= eat_value(open_get_eat_move + enemy_eat)
+        score -= eat_value(open_get_eat_move + enemy_eat) / 10
         # score -= (open_get_eat_move + enemy_eat + 1) ** 7
 
     return score, l_eating_enemy, r_eating_enemy
@@ -293,8 +295,7 @@ def get_diags(board, row_index, col_index):
 @njit
 # @njit("Tuple((int64, List(int64[:])))(int64[:,:], int64[:], boolean, int64, int64, int64)", fastmath=True)
 def get_new_threats(board, position, maximizing_player, player, player_eat, enemy_eat, depth):
-    if print_values:
-        print(position)
+    print(position)
     if not maximizing_player:
         player = player * -1
 
@@ -306,26 +307,10 @@ def get_new_threats(board, position, maximizing_player, player, player_eat, enem
     
     captured_stones = []
 
-    if print_values:
-        print("LR")
-    # result_lr, capture_left_lr, capture_right_lr = check_line(lr_diags, col_index, player, player_eat, enemy_eat)
     result_lr, capture_left_lr, capture_right_lr = check_line(lr_diags, row_index, player, player_eat, enemy_eat)
-    if print_values:
-        print("RL")
     result_rl, capture_left_rl, capture_right_rl = check_line(rl_diags, row_index, player, player_eat, enemy_eat)
-    if print_values:
-        print("ROW")
     result_row, capture_left_row, capture_right_row = check_line(rows, col_index, player, player_eat, enemy_eat)
-    if print_values:
-        print("COL")
     result_col, capture_left_col, capture_right_col = check_line(columns, row_index, player, player_eat, enemy_eat)
-    if print_values:
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
-        print("\n")
 
     score = result_lr + result_rl + result_row + result_col
 
@@ -369,4 +354,3 @@ def get_new_threats(board, position, maximizing_player, player, player_eat, enem
         score *= -1
 
     return score / depth, captured_stones
-    # return score, captured_stones, is_win, line_axis
